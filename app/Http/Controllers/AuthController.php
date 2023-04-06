6dfa585a7493932797fa400fe2bb47482be96313
0xRenegade <therenegadecoding@gmail.com>
2023-04-06 01:03:47 -0500

    add email verification route with method in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,26 +5,65 @@ namespace App\Http\Controllers;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
 use App\Http\Resources\UserResource;
+use App\Mail\EmailVerification;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Str;
+use Nette\Utils\Json;
 
 class AuthController extends Controller
 {
 
-    public function register(RegisterRequest $request): array
+    public function register(Request $request): array
     {
+        if (User::where('email', $request->input('email'))->exists()) {
+            return ['message' => 'user_exists'];
+        }
+
         $user = User::create([
             'name' => $request->input('name'),
             'email' => $request->input('email'),
             'password' => bcrypt($request->input('password')),
         ]);
-        return [
+
+        $payload = Crypt::encrypt([
             'user' => UserResource::make($user),
             'token' => $user->createToken($request->userAgent())->plainTextToken,
+            'verified' => true,
+        ]);
+
+        $message = (new EmailVerification($user->email, $payload));
+
+        Mail::queue($message);
+
+        return ["message" => "registered"];
+    }
+
+    public function verify(Request $request) {
+        $payload = Json::decode($request->getContent());
+        $payload = Crypt::decrypt($payload->payload);
+
+        if (!$payload['verified']) {
+            return [
+                'message' => 'error'
+            ];
+        }
+
+        $user = User::where('email', $payload['user']['email'])->first();
+        $user->email_verified_at = Carbon::now()->format('Y-m-d H:i:s');
+        $user->save();
+
+        return [
+            'user' => $payload['user'],
+            'token' => $payload['token'],
         ];
     }
 
--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/verify', [AuthController::class, 'verify']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
